51d3a29bfc2f4a0cb6b12ab35c5537379f19c43c
cedric <tranthanhnam@outlook.com>
2025-03-05 22:06:50 +0700

    fix: remove unuse code

--- src/configs/index.ts
+++
@@ -1,8 +0,0 @@
-import databaseConfig from './database.config';
-import appConfig from './app.config';
-import redisConfig from './redis.config';
-import authConfig from './auth.config';
-import elasticSearchConfig from "./elastic-search.config";
-
-export * from './config.type';
-export const envConfigs = [databaseConfig, appConfig, redisConfig, authConfig, elasticSearchConfig];
--- src/modules/books/dto/search-book.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
-
-export class SearchBookDto {
-    @IsOptional({ message: 'Name must not be empty' })
-    @IsString({ message: 'Name must be a string' })
-    keyword: string;
-
-}
